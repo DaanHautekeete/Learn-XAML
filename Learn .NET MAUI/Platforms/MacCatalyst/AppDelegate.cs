e1ae6e6a43549ff558883e6aeb614ad59003da1f
Daan.Hautekeete <Daan.Hautekeete@E50401>
2024-11-27 09:13:20 +0100

    Add project to learn XAML

---
+++ Learn .NET MAUI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Learn_.NET_MAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
